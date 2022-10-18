c02b7f9c714e096579523271200c65e2e0b9c6a7
Gilles <gilles1348@gmail.com>
2022-10-18 18:22:14 +0200

    Implemented unbreakable and despawn container options

--- src/main/java/com/gilles_m/rpg_chest/listener/PlayerListener.java
+++ src/main/java/com/gilles_m/rpg_chest/listener/PlayerListener.java
@@ -1,14 +1,17 @@
 package com.gilles_m.rpg_chest.listener;
 
 import com.gilles_m.rpg_chest.container.instance.InstanceManager;
+import com.gilles_m.rpg_chest.event.ContainerCloseEvent;
 import com.gilles_m.rpg_chest.event.ContainerOpenEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PlayerListener implements Listener {
@@ -44,11 +47,25 @@ public class PlayerListener implements Listener {
 		//Check if a player has broken a rpg chest
 		InstanceManager.getInstance().getContainerInstance(event.getBlock().getLocation())
 				.ifPresent(instance -> {
+					if(instance.getContainer().getMetadata().isUnbreakable()) {
+						event.setCancelled(true);
+						return;
+					}
 					if(!instance.isOnCooldown()) {
 						instance.fillInventory();
 						instance.startCooldown();
 					}
 				});
+	}
+
+	@EventHandler
+	protected void onPlayerCloseInventory(final InventoryCloseEvent event) {
+		InstanceManager.getInstance().getContainerInstance(
+				event.getPlayer().getOpenInventory().getTopInventory().getHolder().getInventory().getLocation()
+		).ifPresent(instance ->
+				Bukkit.getServer().getPluginManager()
+						.callEvent(new ContainerCloseEvent(instance, (Player) event.getPlayer()))
+		);
 	}
 
 }
